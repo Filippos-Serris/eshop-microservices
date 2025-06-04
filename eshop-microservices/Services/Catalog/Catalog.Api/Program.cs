26883aa3a530d29e59b6f6754eaa75174149d3e2
Serris Filippos <filipposserris@gmail.com>
2025-06-04 20:22:41 +0300

    Register Carter and MediatR

--- eshop-microservices/Services/Catalog/Catalog.Api/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.Api/Program.cs
@@ -1,9 +1,17 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Confugure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
